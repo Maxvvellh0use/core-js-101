f8120a0a3c0fd30f4f798117f0c7177e3ca62899
Maksim Pavlov <pavlovmark100@gmail.com>
2020-05-11 17:14:57 +0400

    feat: add solutions arrays

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -110,8 +110,9 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  return Math.acos((x1 * x2 + y1 * y2) / (Math.sqrt(x1 * x1 + y1 * y1)
-    * Math.sqrt(x2 * x2 + y2 * y2)));
+  const a = Math.sqrt(x1 * x1 + y1 * y1);
+  const b = Math.sqrt(x2 * x2 + y2 * y2);
+  return Math.acos((x1 * x2 + y1 * y2) / (a * b));
 }
 
 /**
